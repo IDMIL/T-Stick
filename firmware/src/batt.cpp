1e4b5f63734c59b2bf2173a9bef9472ae3570dd4
aburt2 <aniyonsenga@gmail.com>
2023-09-24 17:39:14 -0400

    added software implementation guide and user guide to repo, fixed multipliers for adding vempty and designcap

--- firmware/src/batt.cpp
+++ firmware/src/batt.cpp
@@ -1,12 +1,31 @@
 #include "batt.h"
 
-bool FUELGAUGE::init(int designcap, float rsense, float vempty)
+bool FUELGAUGE::init(fuelgauge_config config, bool reset = true)
 {
-    Wire.beginTransmission(I2CAddress);
+    // Get the parameters and save them
+    i2c_addr = config.i2c_addr;
+    designcap = config.designcap;
+    rsense = config.rsense;
+    vempty = config.vempty;
+    recovery_voltage = config.recovery_voltage;
+
+    // Get Learned parameters
+    if (!reset) {
+        rcomp = config.rcomp;
+        tempco = config.tempco;
+        fullcap = config.fullcap;
+        fullcapnorm = config.fullcapnorm;
+        cycles = config.cycles;
+        raw_soc = config.soc;
+    }
+
+    Wire.beginTransmission(i2c_addr);
     byte error = Wire.endTransmission();
     if (error == 0) //Device Acknowledged
     {
-        bool POR = readReg16Bit(Status)&0x0002;
+        bool POR = readReg16Bit(STATUS_REG)&0x0002;
+        setresistsensor(rsense);  
+        designcap = designcap;
         if (POR)
         {
             while(readReg16Bit(0x3D)&1) {
@@ -20,26 +39,92 @@ bool FUELGAUGE::init(int designcap, float rsense, float vempty)
             writeReg16Bit(0x60, 0x0);
 
             //EZ Config
-            writeReg16Bit(DesignCap, designcap); //Write Design Cap
-            writeReg16Bit(dQAcc, designcap/32); //Write dQAcc
-            writeReg16Bit(Vempty, vempty); //Write Vempty
-            writeReg16Bit(0x46,int(designcap/32)*44138/designcap); //Write dPAcc
-            writeReg16Bit(0xDB, 0x8000); //Write ModelCFG
+            // Write Battery capacity
+            int reg_cap = designcap / capacity_multiplier_mAH;
+            writeReg16Bit(DESIGNCAP_REG, reg_cap); //Write Design Cap
+            writeReg16Bit(dQACC_REG, reg_cap/32); //Write dQAcc
+            writeReg16Bit(dPACC_REG, 44138/32); //Write dPAcc
+
+            // Set empty voltage and recovery voltage
+            // Empty voltage in increments of 10mV
+            int reg_vempty = vempty * 100; //empty voltage in 10mV
+            int reg_recover = 3.88 *25; //recovery voltage in 40mV increments
+            int voltage_settings = (reg_vempty << 7) | reg_recover; 
+            writeReg16Bit(VEMPTY_REG, voltage_settings); //Write Vempty
+            
+            // Set Model Characteristic
+            writeReg16Bit(MODELCFG_REG, 0x8000); //Write ModelCFG
 
             //Wait until model refresh
-            while(readReg16Bit(0xDB)&0x8000) {
+            while(readReg16Bit(MODELCFG_REG)&0x8000) {
                 delay(10);
             }
             //Reload original HbCFG value
             writeReg16Bit(0xBA,HibCFG);
-            writeReg16Bit(Status,readReg16Bit(Status)&0xFFFD); //reset POR Status            
+            writeReg16Bit(STATUS_REG,readReg16Bit(STATUS_REG)&0xFFFD); //reset POR Status            
         }
-        // Set current resistor for calculations
-        setresistsensor(rsense);  
         return true;
     }
     return false; //device not found
 }
+
+// Get Fuel Gauge data functions
+void FUELGAUGE::getsoc() {
+    // Get the State of charge
+    raw_soc = readReg16Bit(REPSOC_REG);
+    cooked_soc = percentage_multiplier * raw_soc;
+}
+
+void FUELGAUGE::getcapacity() {
+    // Get Battery Capacity
+    capacity = capacity_multiplier_mAH * readReg16Bit(REPCAP_REG);
+}
+
+void FUELGAUGE::getvoltage() {
+    // Get voltage
+    voltage = voltage_multiplier_V * readReg16Bit(VCELL_REG);
+    avg_voltage = voltage_multiplier_V * readReg16Bit(AVGVCELL_REG);
+}
+
+void FUELGAUGE::getcurrent(){
+    // Get current
+    current = current_multiplier_mV * readReg16Bit(CURRENT_REG);
+    avg_current = current_multiplier_mV * readReg16Bit(AVGCURRENT_REG);
+}
+
+void FUELGAUGE::getage() {
+    // Get Battery Age
+    age = time_multiplier_Hours * readReg16Bit(AGE_REG);
+}
+
+void FUELGAUGE::gettte() {
+    // Get time to empty
+    tte = time_multiplier_Hours * readReg16Bit(TTE_REG);
+}
+
+void FUELGAUGE::getparameters() {
+    // Get learned parameters
+    rcomp = readReg16Bit(RCOMPP0_REG);
+    tempco = readReg16Bit(TEMPCO_REG);
+    fullcap = readReg16Bit(FULLCAP_REG);
+    fullcapnorm = readReg16Bit(FULLCAPNORM_REG);
+    cycles = readReg16Bit(CYCLES_REG);
+}
+
+void FUELGAUGE::getBatteryData() {
+    // Read all the battery data from the registers
+    getsoc();
+    getcapacity();
+    getvoltage();
+    getcurrent();
+    getage();
+    gettte();
+
+    // Get Learned parameters
+    getparameters();
+}
+
+// Private Methods
 
 void FUELGAUGE::setresistsensor(float rsense) {
     resistSensor = rsense;
@@ -52,52 +137,50 @@ void FUELGAUGE::updateMultipliers() {
     current_multiplier_mV = (1.5625e-3)/resistSensor; //refer to row "Current"
 }
 
-float FUELGAUGE::getresistsensor() {
-    return resistSensor;
-}
-
-void FUELGAUGE::getBatteryData() {
-    raw_soc = percentage_multiplier * readReg16Bit(RepSOC);
-    cooked_soc = readReg16HighBit(RepSOC);
-    capacity = capacity_multiplier_mAH * readReg16Bit(RepCap);
-    voltage = voltage_multiplier_V * readReg16Bit(VCell);
-    current = current_multiplier_mV * readReg16Bit(Current);
-    tte = time_multiplier_Hours * readReg16Bit(TimeToEmpty);
-    age = time_multiplier_Hours * readReg16Bit(Age);
-}
-// Private Methods
 
 void FUELGAUGE::writeReg16Bit(uint8_t reg, uint16_t value)
 {
   //Write order is LSB first, and then MSB. Refer to AN635 pg 35 figure 1.12.2.5
-  Wire.beginTransmission(I2CAddress);
+  Wire.beginTransmission(i2c_addr);
   Wire.write(reg);
   Wire.write( value       & 0xFF); // value low byte
   Wire.write((value >> 8) & 0xFF); // value high byte
   uint8_t last_status = Wire.endTransmission();
 }
 
+bool FUELGAUGE::writeVerifyReg16Bit(uint8_t reg, uint16_t value)
+{
+  //Write order is LSB first, and then MSB. Refer to AN635 pg 35 figure 1.12.2.5
+  Wire.beginTransmission(i2c_addr);
+  Wire.write(reg);
+  Wire.write( value       & 0xFF); // value low byte
+  Wire.write((value >> 8) & 0xFF); // value high byte
+  // Wait a bit
+  delay(10);
+  int attempt = 0;
+  // Verify that the value has been written before moving on
+  while (value != readReg16Bit(reg) && attempt < 10) {
+    delay(10);
+    attempt++;
+  };
+  uint8_t last_status = Wire.endTransmission();
+  
+  if (attempt > 10) {
+    return false;
+  } else {
+    return true;
+  }
+}
+
 uint16_t FUELGAUGE::readReg16Bit(uint8_t reg)
 {
   uint16_t value = 0;  
-  Wire.beginTransmission(I2CAddress); 
+  Wire.beginTransmission(i2c_addr); 
   Wire.write(reg);
   uint8_t last_status = Wire.endTransmission(false);
   
-  Wire.requestFrom(I2CAddress, (uint8_t) 2); 
+  Wire.requestFrom(i2c_addr, (uint8_t) 2); 
   value  = Wire.read();
   value |= (uint16_t)Wire.read() << 8;      // value low byte
-  return value;
-}
-
-uint16_t FUELGAUGE::readReg16HighBit(uint8_t reg)
-{
-  uint16_t value = 0;  
-  Wire.beginTransmission(I2CAddress); 
-  Wire.write(reg);
-  uint8_t last_status = Wire.endTransmission(false);
-  
-  Wire.requestFrom(I2CAddress, (uint8_t) 2); 
-  value  = Wire.read();
   return value;
 }
--- firmware/src/batt.h
+++ firmware/src/batt.h
@@ -9,51 +9,96 @@ class FUELGAUGE
         // register addresses 
         enum regAddr
         {
-        Status      = 0x00, //Maintains all flags related to alert thresholds and battery insertion or removal.
-        Age         = 0x07, //calculated percentage value of capacity compared to original design capacity.
-        Temperature = 0x08, //Temperature of MAX17055 chip
-        VCell       = 0x09, //VCell reports the voltage measured between BATT and CSP.
-        AvgVCell    = 0x19, //The AvgVCell register reports an average of the VCell register readings. 
-        Current     = 0x0A, //Voltage between the CSP and CSN pins, and would need to convert to current
-        AvgCurrent  = 0x0B, //The AvgCurrent register reports an average of Current register readings
-        RepSOC      = 0x06, //The Reported State of Charge of connected battery. Refer to AN6358 page 23 and 13
-        RepCap      = 0x05, //Reported Capacity. Refer to page 23 and 13 of AN6358.
-        TimeToEmpty = 0x11, //How long before battery is empty (in ms). Refer to page 24 and 13 of AN6358 
-        DesignCap   = 0x18, //Capacity of battery inserted, not typically used for user requested capacity
-        Vempty      = 0x3A, //Register for voltage when the battery is empty
-        dQAcc      = 0x45 //Register for voltage when the battery is empty
+        STATUS_REG      = 0x00, //Maintains all flags related to alert thresholds and battery insertion or removal.
+        AGE_REG         = 0x07, //calculated percentage value of capacity compared to original design capacity.
+        TEMP_REG        = 0x08, //Temperature of MAX17055 chip
+        VCELL_REG       = 0x09, //VCell reports the voltage measured between BATT and CSP.
+        AVGVCELL_REG    = 0x19, //The AvgVCell register reports an average of the VCell register readings. 
+        CURRENT_REG     = 0x0A, //Voltage between the CSP and CSN pins, and would need to convert to current
+        AVGCURRENT_REG  = 0x0B, //The AvgCurrent register reports an average of Current register readings
+        REPSOC_REG      = 0x06, //The Reported State of Charge of connected battery. Refer to AN6358 page 23 and 13
+        REPCAP_REG      = 0x05, //Reported Capacity. Refer to page 23 and 13 of AN6358.
+        TTE_REG         = 0x11, //How long before battery is empty (in ms). Refer to page 24 and 13 of AN6358 
+        DESIGNCAP_REG   = 0x18, //Capacity of battery inserted, not typically used for user requested capacity
+        VEMPTY_REG      = 0x3A, //Register for voltage when the battery is empty
+        dQACC_REG       = 0x45, //Register for dQAcc
+        dPACC_REG       = 0x46, //Register for dPAcc
+        MODELCFG_REG    = 0xDB, // Register for MODELCFG
+        RCOMPP0_REG     = 0x38, // Register for learned parameter rcomp0
+        TEMPCO_REG      = 0x39, // Register for learned parameter tempco
+        FULLCAP_REG     = 0x10, // Register for learned parameter full capacity
+        CYCLES_REG      = 0x17, // Register for learned parameter charge cycles
+        FULLCAPNORM_REG = 0x23, // Register for learned parameter full capacity (normalised)
         };
+        // Fuel Gauge Config
+        struct fuelgauge_config {
+            // Initialisation Elements
+            uint8_t i2c_addr = 0x36;
+            int designcap = 2400;
+            int rsense = 0.01;
+            int vempty = 3.3;
+            int recovery_voltage = 3.88;
+
+            // Learned Parameters
+            int soc = 0;
+            int rcomp = 0;
+            int tempco = 0;
+            int fullcap = 0;
+            int fullcapnorm = 0;
+            int cycles = 0;
+        };
+        // Battery Parameters
+        // Initialisation Elements
+        uint8_t i2c_addr = 0x36;
+        int designcap = 2400;
+        int rsense = 0.01;
+        int vempty = 3.3;
+        int recovery_voltage = 3.88;
+
         // raw variables
-        uint16_t current = 0;
+        float current = 0;
+        float avg_current = 0;
         float voltage = 0;
-        uint16_t capacity = 0;
+        float avg_voltage = 0;
+        int capacity = 0;
+        int designcap = 0;
 
-        // SOC
-        uint16_t raw_soc = 0;
-        uint16_t cooked_soc = 0; 
+        // learned variables
+        int rcomp = 0;
+        int tempco = 0;
+        int fullcap = 0;
+        int cycles = 0;
+        int fullcapnorm = 0;
 
-        // Learned variables
-        uint16_t age = 0;
-        uint16_t tte = 0;
-        uint16_t chargecycles = 0;
-        uint16_t fullcapnorm = 0;
-
+        // SOC, age and time to empty
+        int raw_soc = 0; // State of charge
+        int cooked_soc = 0; //
+        int age = 0;
+        int tte = 0;
 
         // methods
-        bool init(int designcap, float rsense, float vempty);
-        void setresistsensor(float rsense);
-        float getresistsensor();
-        void updateMultipliers();
+        // Initialise Fuel Gauge
+        bool init(fuelgauge_config config, bool reset = true);
+        
+        // Get properties
+        void getsoc();
+        void getcapacity();
+        void getvoltage();
+        void getcurrent();
+        void getage();
+        void gettte();
         void getBatteryData();
+
+        // Save learned parameters
+        void getparameters();
 
     private:
         //variables
         float resistSensor = 0.01; //default internal resist sensor
-        uint8_t I2CAddress = 0x36;
+        uint8_t i2c_addr = 0x36;
         uint16_t HibCFG = 0;
         
-        //Based on "Standard Register Formats" AN6358, figure 1.3. 
-        //Multipliers are constants used to multiply register value in order to get final result
+        //Based on "Register Resolutions from MAX17055 Technical Reference" Table 6. 
         float capacity_multiplier_mAH = (5e-3)/resistSensor; //refer to row "Capacity"
         float current_multiplier_mV = (1.5625e-3)/resistSensor; //refer to row "Current"
         float voltage_multiplier_V = 7.8125e-5; //refer to row "Voltage"
@@ -61,7 +106,9 @@ class FUELGAUGE
         float percentage_multiplier = 1.0/256.0; //refer to row "Percentage"
         
         //methods
+        void setresistsensor(float rsense);
+        void updateMultipliers();
         uint16_t readReg16Bit(uint8_t reg);
-        uint16_t readReg16HighBit(uint8_t reg);
         void writeReg16Bit(uint8_t reg, uint16_t value);
+        bool writeVerifyReg16Bit(uint8_t reg, uint16_t value);
 };
